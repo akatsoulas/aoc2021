07bdd280f7cfc517862b81e9d5976a9506d9fb9d
Tasos Katsoulas <akatsoulas@gmail.com>
2021-12-07 13:41:12 +0200

    include day2 part 2

--- src/day2.py
+++ src/day2.py
@@ -1,17 +1,18 @@
 def get_position(data):
-    horizontal, depth = 0, 0
+    horizontal, depth, aim = 0, 0, 0
     for loc in data:
         match loc.split(" "):
             case ["forward", x]:
                 horizontal += int(x)
+                depth += int(x) * aim
             case ["up", x]:
-                depth -= int(x)
+                aim -= int(x)
             case ["down", x]:
-                depth += int(x)
-    return horizontal * depth
+                aim += int(x)
+    return (horizontal * aim, horizontal * depth)
 
 
 if __name__ == "__main__":
-    with open("../inputs/day2p1") as f:
+    with open("../inputs/day2") as f:
         data = f.read().splitlines()
     print(get_position(data))
